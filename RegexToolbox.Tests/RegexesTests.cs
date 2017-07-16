3174e7e68038528eaf296120047ff4c4e58a2e76
Mark Whitaker <mark@mainwave.co.uk>
2017-07-16 08:18:41 +0100

    Added Regexes class. Added Regexes.Ipv4Address. Added unit tests.

---
+++ RegexToolbox.Tests/RegexesTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace RegexToolbox.Tests
+{
+    [TestFixture]
+    public class RegexesTests
+    {
+        [Test]
+        public void TestIpv4Address()
+        {
+            var validIpAddresses = new[]
+            {
+                "0.0.0.0",
+                "10.0.0.1",
+                "127.0.0.0",
+                "255.255.255.0",
+                "255.255.255.255"
+            };
+
+            var invalidIpAddresses = new[]
+            {
+                "0.0.0",
+                "255.255.255.256",
+                "elephant"
+            };
+
+            foreach (var validIpAddress in validIpAddresses)
+            {
+                var match = Regexes.Ipv4Address.Match(validIpAddress);
+                Assert.IsTrue(match.Success);
+                Assert.AreEqual(5, match.Groups.Count);
+            }
+
+            foreach (var invalidIpAddress in invalidIpAddresses)
+            {
+                var match = Regexes.Ipv4Address.Match(invalidIpAddress);
+                Assert.IsFalse(match.Success);
+            }
+        }
+    }
+}
---
+++ RegexToolbox/Regexes.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexToolbox
+{
+    public static class Regexes
+    {
+        /// <summary>
+        /// Matches valid IP addresses (0.0.0.0 to 255.255.255.255) and captures each
+        /// of the four parts in a group
+        /// </summary>
+        public static readonly Regex Ipv4Address = new Regex(
+            @"\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\." +
+            @"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\." +
+            @"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\." +
+            @"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\b");
+    }
+}
